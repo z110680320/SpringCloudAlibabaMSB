3cfa57d0326ea7d637ff46fee05be0a775baaeff
xiaoxin <110680320@qq.com>
2022-07-25 16:35:09 +0800

    问题解决  nacos节点缺失  springboot启动类上注解顺序错误

--- cloudalibaba-config-3377/src/main/java/com/mashibing/cloudalibabaconfig3377/CloudalibabaConfig3377Application.java
+++ cloudalibaba-config-3377/src/main/java/com/mashibing/cloudalibabaconfig3377/CloudalibabaConfig3377Application.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+
 @EnableDiscoveryClient
+@SpringBootApplication
+
 public class CloudalibabaConfig3377Application {
 
     public static void main(String[] args) {
--- cloudalibaba-config-3377/src/main/java/com/mashibing/cloudalibabaconfig3377/controller/DemoController.java
+++ cloudalibaba-config-3377/src/main/java/com/mashibing/cloudalibabaconfig3377/controller/DemoController.java
@@ -2,17 +2,19 @@ package com.mashibing.cloudalibabaconfig3377.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
 public class DemoController {
-@Value("${config.info}")
-String info;
-    @RequestMapping("helloNacos")
-    public String helloNacos(){
-        return info;
+   @Value("${config.info}")
+    private String configInfo;
 
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
     }
+
 }
